34f0de0f6246cc89745c67b738dd55b7fc57e728
Brian Perry <contact.brianperry@gmail.com>
2018-04-06 17:00:00 -0500

    Initial attempt at static build

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,5 +4,29 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import ReactDOMServer from 'react-dom/server';
+
+//const React = require('react');
+//const ReactDOM = require('react-dom');
+//const ReactDOMServer = require('react-dom/server');
+//const App = require('./components/App');
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
+
+// Getting there...
+// https://jaketrent.com/post/react-routing-static-site-browser/
+
+if (typeof global.document !== 'undefined') {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  registerServiceWorker();
+}
+
+export default locals =>
+  ReactDOMServer.renderToString(
+    <html>
+      <div>
+        <h1>{locals.path}</h1>
+      </div>
+    </html>
+  )
